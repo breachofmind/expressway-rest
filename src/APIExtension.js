f23d7080840148ec58dd6b39134522e003b113f5
Mike Adamczyk <mike.adamczyk@brightstar.com>
2017-02-10 11:12:27 -0600

    Move routes to config

--- src/APIExtension.js
+++ src/APIExtension.js
@@ -25,30 +25,7 @@ class APIExtension extends Extension
             require('./controllers/RESTController'),
         ]);
 
-        this.routes.middleware([
-            'Init',
-            'APIRequest',
-            'Localization',
-            'BodyParser',
-            'Session',
-            'BasicAuth'
-        ]);
-
-        this.routes.add([
-            {
-                "GET    /"              : 'RESTController.index',
-                "GET    /locale"        : 'RESTController.locale',
-                "GET    /:model"        : 'RESTController.fetchAll',
-                "POST   /auth"          : 'RESTController.auth',
-                "POST   /:model"        : 'RESTController.create',
-                "POST   /:model/search" : 'RESTController.search',
-                "GET    /:model/:id"    : 'RESTController.fetchOne',
-                "PUT    /:model/:id"    : 'RESTController.update',
-                "DELETE /:model/:id"    : 'RESTController.trash',
-            }
-        ]);
-
-        this.routes.error(404, 'APINotFound');
+        this.routes.use(require('./config/routes'));
     }
 
     /**
---
+++ src/config/routes.js
@@ -0,0 +1,24 @@
+module.exports = {
+    middleware: [
+        'Init',
+        'APIRequest',
+        'Localization',
+        'BodyParser',
+        'Session',
+        'BasicAuth'
+    ],
+    paths: [
+        {
+            "GET    /"              : 'RESTController.index',
+            "GET    /locale"        : 'RESTController.locale',
+            "GET    /:model"        : 'RESTController.fetchAll',
+            "POST   /auth"          : 'RESTController.auth',
+            "POST   /:model"        : 'RESTController.create',
+            "POST   /:model/search" : 'RESTController.search',
+            "GET    /:model/:id"    : 'RESTController.fetchOne',
+            "PUT    /:model/:id"    : 'RESTController.update',
+            "DELETE /:model/:id"    : 'RESTController.trash',
+        }
+    ],
+    error: "APINotFound"
+};
